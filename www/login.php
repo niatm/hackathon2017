cbb34a4995d3322086c86478e4cc5bf0f09a5e55
Ir. Mohd Nazrul Hanif Bin Nordin <nazrul.nordin@tm.com.my>
2017-08-09 15:58:07 +0800

    upload qrcode.php

--- www/login.php
+++ www/login.php
@@ -3,7 +3,6 @@
 $username = $_REQUEST['username'];
 $password = $_REQUEST['password'];
 
-
 $url = "http://172.30.201.238:8080/api/ldap/$username/$password";
 $result = file_get_contents($url);
 echo $result;
