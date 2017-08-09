8306a37e2213341ae4ad153f0c72ed56e8c49beb
Ir. Mohd Nazrul Hanif Bin Nordin <nazrul.nordin@tm.com.my>
2017-08-09 16:21:42 +0800

    updated elevator.php

--- www/elevator.php
+++ www/elevator.php
@@ -1,6 +1,6 @@
 <?php
 
-$url = "http://itnthackathon.bweas.tm.com.my/api/getElevatorInfo";
+$url = "http://172.30.201.238:8080/api/getElevatorInfo";
 $json = file_get_contents($url);
 //$json_data = json_decode($json, true);
 echo $json;
