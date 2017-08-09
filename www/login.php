a2870ce71d6e19e350a965804c55f0244986dfe5
Ir. Mohd Nazrul Hanif Bin Nordin <nazrul.nordin@tm.com.my>
2017-08-09 15:18:20 +0800

    updated login.php

--- www/login.php
+++ www/login.php
@@ -1,6 +1,10 @@
 <?php
 
-$url = "http://172.30.201.238:8080/api/ldap/tm36186/Nazrul7";
+$username = $_GET['username'];
+$password = $_GET['password'];
+
+
+$url = "http://172.30.201.238:8080/api/ldap/$username/$password";
 $result = file_get_contents($url);
 echo $result;
 
