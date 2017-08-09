c10ecc78d77c914d8e5e2ef955b76e0aa1eb1b40
Ir. Mohd Nazrul Hanif Bin Nordin <nazrul.nordin@tm.com.my>
2017-08-09 14:25:39 +0800

    updated director.php

--- www/director.php
+++ www/director.php
@@ -1,5 +1,8 @@
 <?php
 
-echo "helo world";
+$url = "http://itnthackathon.bweas.tm.com.my/api/getElevatorInfo";
+$json = file_get_contents($url);
+$json_data = json_decode($json, true);
+echo "My token: ". $json_data;
 
 ?>
