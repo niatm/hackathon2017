72bcc98ad9aa6dc629e3ab959cecae39c2e1e9e8
Ir. Mohd Nazrul Hanif Bin Nordin <nazrul.nordin@tm.com.my>
2017-08-09 19:58:53 +0800

    upload profile.php

--- www/profile.php
+++ www/profile.php
@@ -2,7 +2,7 @@
 
 $username = $_REQUEST['username'];
 
-$url = "http://172.30.201.238:8080/api/getStaffProfile?filter=STAFF_NUMBER,eq,$username";
+$url = "http://172.30.201.238:8080/api/getStaffProfile?filter=STAFF_NUMBER,eq,$username&transform=1";
 $json = file_get_contents($url);
 //$json_data = json_decode($json, true);
 echo $json;
