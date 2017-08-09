9aa4ddcdc70e9762808e8ae4ed3b62a3cc4e247a
Ir. Mohd Nazrul Hanif Bin Nordin <nazrul.nordin@tm.com.my>
2017-08-10 03:16:53 +0800

    updated

--- www/qrcode.php
+++ www/qrcode.php
@@ -4,7 +4,7 @@ $username = $_REQUEST['username'];
 $from = $_REQUEST['from'];
 $to = $_REQUEST['to'];
 
-$url = "http://175.139.131.98/updateinfo.php?user=$username&from=20&to=G&timer=30";
+$url = "http://175.139.131.98/updateinfo.php?user=$username&from=$from&to=$to&timer=30";
 $result = file_get_contents($url);
 
 echo $_SERVER['SERVER_NAME'] .'/www/qr/test.png';
