39f902b9566b76cc4f475d81785be800b84f922c
Ir. Mohd Nazrul Hanif Bin Nordin <nazrul.nordin@tm.com.my>
2017-08-10 03:31:24 +0800

    updated

--- www/qrcode.php
+++ www/qrcode.php
@@ -4,7 +4,10 @@ $username = $_REQUEST['username'];
 $from = $_REQUEST['from'];
 $to = $_REQUEST['to'];
 
-$url = "http://175.139.131.98/updateinfo.php?user=$username&from=$from&to=$to&timer=30";
+$url = "http://172.30.201.238:8080/api/getStaffProfile?filter=STAFF_NUMBER,eq,$username&transform=1";
+$fullname = file_get_contents($url);
+
+$url = "http://175.139.131.98/updateinfo.php?user=$fullname&from=$from&to=$to&timer=30";
 $result = file_get_contents($url);
 
 echo $_SERVER['SERVER_NAME'] .'/www/qr/test.png';
