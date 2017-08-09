b8117113ad6d720233921d1a26eecd17d7a8b31a
Ir. Mohd Nazrul Hanif Bin Nordin <nazrul.nordin@tm.com.my>
2017-08-09 22:24:21 +0800

    updated profile.php

--- www/elevator.php
+++ www/elevator.php
@@ -1,8 +1,17 @@
 <?php
 
+$username = $_REQUEST['username'];
+$from = $_REQUEST['from'];
+$to = $_REQUEST['to'];
+
 $url = "http://172.30.201.238:8080/api/getElevatorInfo";
 $json = file_get_contents($url);
 //$json_data = json_decode($json, true);
-echo $json;
+echo '
+	{
+	  "liftnum": "MTM1",
+	  "eta": "20"
+	}
+';
 
 ?>
